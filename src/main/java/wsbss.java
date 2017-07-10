85344ca76828d2e19bd143092e84a033cc351d13
legite <vastlee@hotmail.com>
2017-07-10 14:03:32 +0800

    i was silly

---
+++ src/main/java/wsbss.java
@@ -0,0 +1,22 @@
+
+public class wsbss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 0;
+		while (n < 1000) {
+			if (n == 0) {
+				n ++;
+			}else{
+				int s = 0;
+				for(int i = 0; i < n; i ++){
+					s = s + i;
+				}
+				System.out.println(s);
+				n ++;
+			}
+		}
+		System.out.println("Wo is not is sha!!");
+	}
+
+}
