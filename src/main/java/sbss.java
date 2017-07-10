65b376cb2f2c84808999d5eea81e07bf6c1c4ef4
le <liyfeenx@163.com>
2017-07-10 13:28:51 +0800

    really red

---
+++ src/main/java/sbss.java
@@ -0,0 +1,11 @@
+public class sbss{
+	private int sb;
+	private int sc;
+	public void hello() {
+		System.out.println("Hello,world!");
+		this.sb = 2;
+		this.sc = 3;
+		System.out.println(sb);
+		System.out.println(sc);
+	}
+}
